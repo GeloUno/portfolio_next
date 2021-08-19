b37c39c203d7917912d8765c4d2b8bcf01fd7856
GeloUno <geloke59@gmail.com>
2021-08-19 17:21:28 +0200

    stories config to use tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,13 +2,16 @@ const path = require('path')
 module.exports = {
 
   stories: [
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: [
-    // "@storybook/addon-postcss",
+    "@storybook/addon-postcss",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    'storybook-css-modules-preset',
+    // '@storybook/preset-create-react-app'
+    // '@storybook/preset-scss'
   ],
   webpackFinal: async (config) => {
     config.module.rules.push({
@@ -17,13 +20,19 @@ module.exports = {
         {
           loader: 'postcss-loader',
           options: {
+            postcssLoaderOptions: {
+              implementation: require('postcss'),
+            },
             ident: 'postcss',
             plugins: [
+              require('postcss-import'),
+              require('tailwindcss/nesting'),
               require('tailwindcss'),
-              require('autoprefixer')
+              require('autoprefixer'),
             ]
           }
-        }
+        },
+
       ],
       include: path.resolve(__dirname, '../'),
     })
