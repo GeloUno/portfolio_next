37705d9f0bdac1759f65cd5d7efde78241338d90
GeloUno <geloke59@gmail.com>
2021-09-01 14:46:20 +0200

    project desciption is arrary too

--- models/IProject.tsx
+++ models/IProject.tsx
@@ -2,7 +2,7 @@ import { ETechnology } from "./ETechnology";
 
 export interface IProject {
     title: string;
-    description: string;
+    description: string | Array<string>;
     technology: Array<ETechnology>;
     github: string;
     image?: string;
