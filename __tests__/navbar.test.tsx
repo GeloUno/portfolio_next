f4680f4de28379363eead9e5865d344406702dc8
GeloUno <geloke59@gmail.com>
2021-09-04 15:09:42 +0200

    navbar testing

---
+++ __tests__/navbar.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+
+import Navbar from '../components/navbar/navbar';
+
+jest.mock('next/router', () => ({
+    useRouter() {
+        return {
+            push: jest.fn(),
+            back: jest.fn(),
+            pathname: '/'
+        }
+    }
+}))
+
+
+describe('NavBar', () => {
+
+    it('should render', async () => {
+        expect(render(<Navbar />)).toBeTruthy()
+    })
+
+
+});
