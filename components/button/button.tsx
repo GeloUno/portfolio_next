8c900b4fe13f0140c5ae5b6d87c34f1f1969127a
Grzegorz Kędziora <geloke59@gmail.com>
2023-09-14 14:27:31 +0200

    add linkedin and github in contact

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,80 +1,110 @@
 import Link from 'next/link';
 import { HTMLAttributes } from 'react';
-import classes from './button.module.css'
+import classes from './button.module.css';
+import Image from 'next/image';
 
 export enum ButtonClassEnum {
-    PRIMARY = 'myBtnColorPrimary',
-    SECONDARY = 'myBtnColorSecondary'
+  PRIMARY = 'myBtnColorPrimary',
+  SECONDARY = 'myBtnColorSecondary',
 }
 
 export interface IMailTo {
-    email: string,
-    subject?: string,
-
+  email: string;
+  subject?: string;
 }
 
 export interface IButtonProps {
-    labelButton: string,
-    classButton: ButtonClassEnum,
-    className?: string,
-    onClick?: Function,
-    link?: string,
-    mailto?: IMailTo,
+  labelButton?: string;
+  classButton: ButtonClassEnum;
+  className?: string;
+  onClick?: Function;
+  link?: string;
+  mailto?: IMailTo;
+  icon?: 'linkedin' | 'mail' | 'github';
+  cyrcle?: boolean;
 }
 
 function Button({
-    labelButton,
-    classButton,
-    onClick,
-    link,
-    className,
-    mailto }: IButtonProps) {
-
-    function onClickHandler(event: React.MouseEvent<HTMLButtonElement>) {
-        event.preventDefault()
-        onClick && onClick()
-    }
-    if (mailto) {
-        return (
-            <Link href='#'>
-                <a
-                    href={`mailto:${mailto.email} ${mailto.subject && `?subject=${mailto.subject}`}`}
-                    target='_blank'
-                    data-testid="buttonLinkMailTo"
-                    className={`${classes.mybtn} ${classes[classButton]} ${className ? className : ""} `}
-
-                > {labelButton}</a>
-            </Link>
-        )
-    }
-
-    if (link) {
-        return (
-            <Link href={`${link}`}
-                data-testid="button" >
-                {/* <a className={`${classes.mybtn} ${classes[classButton]} ${className} 
-                transform hover:scale-105 motion-reduce:transform-none
-                `}> */}
-                <a data-testid="buttonLink"
-                    className={`${classes.mybtn} ${classes[classButton]} ${className ? className : ""} `}
-                >
-                    {labelButton}</a>
-            </Link>
-        )
-    }
-
+  labelButton,
+  classButton,
+  onClick,
+  link,
+  className,
+  mailto,
+  icon,
+  cyrcle,
+}: IButtonProps) {
+  function onClickHandler(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    onClick && onClick();
+  }
+  if (mailto) {
     return (
-        <button data-testid="button"
-            // className={`transform hover:scale-105 motion-reduce:transform-none`}
-            onClick={(event) => { onClickHandler(event) }}
+      <Link href="#">
+        <a
+          href={`mailto:${mailto.email} ${
+            mailto.subject && `?subject=${mailto.subject}`
+          }`}
+          target="_blank"
+          data-testid="buttonLinkMailTo"
+          className={`${cyrcle ? classes.mybtncyrcle : classes.mybtn} ${
+            classes[classButton]
+          } ${className ? className : ''} `}
         >
-            <span
-                data-testid="buttonContext"
-                className={`${classes.mybtn} ${classes[classButton]} ${className ? className : ""}`}>
-                {labelButton}
-            </span>
-        </button>
+          {icon && (
+            <Image
+              src={`/icons/${icon}.svg`}
+              width={25}
+              height={25}
+              className="mybtn"
+              style={{ display: 'list-item' }}
+            />
+          )}
+          {labelButton && labelButton}
+        </a>
+      </Link>
     );
+  }
+  if (link) {
+    return (
+      <Link href={`${link}`} data-testid="button">
+        <a
+          data-testid="buttonLink"
+          className={`${cyrcle ? classes.mybtncyrcle : classes.mybtn} ${
+            classes[classButton]
+          } ${className ? className : ''} `}
+        >
+          {icon && (
+            <Image
+              src={`/icons/${icon}.svg`}
+              width={25}
+              height={25}
+              style={{ display: 'list-item' }}
+            />
+          )}
+          {labelButton && labelButton}
+        </a>
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      data-testid="button"
+      onClick={(event) => {
+        onClickHandler(event);
+      }}
+    >
+      <span
+        data-testid="buttonContext"
+        className={`${classes.mybtn} ${classes[classButton]} ${
+          className ? className : ''
+        }`}
+      >
+        {labelButton}
+      </span>
+    </button>
+  );
 }
 
 export default Button;
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,17 +1,14 @@
-
-
-import { Fragment } from 'react';
 import Button, { ButtonClassEnum } from '../components/button/button';
-import Link from 'next/link';
 import Container from '../components/shared/container';
 import { IMailTo } from '../components/button/button';
 
 const mailto: IMailTo = {
-    email: 'ge.kedziora@gmail.com',
-    subject: 'Contact to make cooperation'
-}
-const contact =
-    (<div className="flex-col        
+  email: 'ge.kedziora@gmail.com',
+  subject: 'Contact to make cooperation',
+};
+const contact = (
+  <div
+    className="flex-col        
     text-cyan-600 
     bg-cyan-100 
     my-5  
@@ -22,36 +19,49 @@ const contact =
     max-w-2xl
     items-center  
     "
-    // bg-opacity-60
-    // style={{ backdropFilter: "blur(20px)" }}
-    >
-        <h3 className='p-7'>I am always open to make new business relationships. Please just send me an email. I guarantee I will send You an answer.
-        </h3>
+  >
+    <h3 className="p-7">
+      I am always open to make new business relationships. Please just send me
+      an email. I guarantee I will send You an answer.
+    </h3>
 
-        <div className='flex justify-center p-7'>
-
-            <Button
-                classButton={ButtonClassEnum.PRIMARY}
-                labelButton='Send email'
-                mailto={mailto}
-            />
-        </div>
-
-        <p className='p-7 text-xs'>
-            *if you have trouble sending an email.
-            For example the email client doesn't want to open. Please make sure you have set your default email client in windows 10.
-        </p>
+    <div className="flex justify-center p-7">
+      <Button
+        classButton={ButtonClassEnum.PRIMARY}
+        icon="linkedin"
+        link="https://www.linkedin.com/in/grzegorz-k%C4%99dziora-6b7374147/"
+        cyrcle
+      />
+      <Button
+        classButton={ButtonClassEnum.PRIMARY}
+        icon="github"
+        link="https://github.com/GeloUno"
+        cyrcle
+      />
+      <Button
+        classButton={ButtonClassEnum.PRIMARY}
+        icon="mail"
+        mailto={mailto}
+        cyrcle
+      />
     </div>
-    )
+
+    <p className="p-7 text-xs">
+      *if you have trouble sending an email. For example the email client
+      doesn't want to open. Please make sure you have set your default email
+      client in windows 10.
+    </p>
+  </div>
+);
 
 function ContactPage() {
-    return (
-        <Container
-            title={'Contact'}
-            children={contact}
-            switchHorizontalView={true}
-        />
-    );
+  return (
+    <Container
+      title={'Contact'}
+      children={contact}
+      switchHorizontalView={true}
+    />
+  );
 }
 
 export default ContactPage;
