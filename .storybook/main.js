a6064c624551d77a12ec0aa6bd21412c7b2184fb
Grzegorz Kędziora <geloke59@gmail.com>
2023-09-14 14:27:12 +0200

    cleaning

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,14 @@
-const path = require('path')
+const path = require('path');
 module.exports = {
-
   stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-postcss",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    '@storybook/addon-postcss',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
     'storybook-css-modules-preset',
-    // '@storybook/preset-create-react-app'
-    // '@storybook/preset-scss'
   ],
   webpackFinal: async (config) => {
     config.module.rules.push({
@@ -29,14 +26,12 @@ module.exports = {
               require('tailwindcss/nesting'),
               require('tailwindcss'),
               require('autoprefixer'),
-            ]
-          }
+            ],
+          },
         },
-
       ],
       include: path.resolve(__dirname, '../'),
-    })
-    return config
+    });
+    return config;
   },
-
-}
+};
